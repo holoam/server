4a2f1e396ef3cbed459859783a6937eebba59498
Riad Benguella <benguella@gmail.com>
2016-01-23 20:08:25 +0100

    chore(various): Small refactos + Testing

---
+++ test/utils/urlGenerator.spec.js
@@ -0,0 +1,16 @@
+import expect from 'expect'
+import UrlGenerator from '../../lib/utils/urlGenerator';
+import App from '../../lib/models/app';
+import Release from '../../lib/models/release';
+
+describe('UrlGenerator', () => {
+    it('generate the right url for a release', () => {
+        const baseUrl = 'http://baseUrl',
+            generator = new UrlGenerator(baseUrl),
+            app = new App('app'),
+            release = new Release('v1', 'notes', new Date());
+
+        expect(generator.generateDownloadReleaseUrl(app, release))
+            .toEqual('http://baseUrl/app/v1/update')
+    })
+})
