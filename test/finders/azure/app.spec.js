86527ba79ff8a4daf989293874af1affb79ea59d
jubianchi <contact@jubianchi.fr>
2016-01-25 15:06:42 +0100

    test(finders): Add test on Azure finders

---
+++ test/finders/azure/app.spec.js
@@ -0,0 +1,50 @@
+import "babel-polyfill";
+import expect from "expect";
+import AzureAppFinder from "../../../lib/finders/azure/app";
+import pkgcloud from "pkgcloud";
+
+describe("AzureAppFinder", () => {
+    let finder, client;
+
+    beforeEach(() => client = { getContainer: (_, done) => done(null, null), getContainers: (done) => done(null, []) });
+    beforeEach(() => expect.spyOn(pkgcloud.storage, "createClient").andReturn(client));
+    beforeEach(() => finder = new AzureAppFinder("foo", "bar"));
+
+    describe("app", () => {
+        describe("when app exists", () => {
+            it("should return app data", async () => {
+                expect.spyOn(client, "getContainer").andCall((_, done) => done(null, { name: "todo" }));
+
+                expect(await finder.app("todo")).toEqual({ name: "todo" });
+            });
+        });
+
+        describe("when app does not exist", () => {
+            it("should return null if there is an error", async () => {
+                expect.spyOn(client, "getContainer").andCall((_, done) => done(new Error()));
+
+                expect(await finder.app("todo")).toBe(null);
+            });
+        });
+    });
+
+    describe("apps", () => {
+        it("should return app list", async() => {
+            expect.spyOn(client, "getContainers").andCall(done => done(null, [{ name: "todo" }, { name: "neutron" }]));
+
+            expect(await finder.apps()).toEqual([{ name: "todo" }, { name: "neutron" }]);
+        });
+
+        it("should return an empty app list", async() => {
+            expect.spyOn(client, "getContainers").andCall((done) => done(null, []));
+
+            expect(await finder.apps()).toEqual([]);
+        });
+
+        it("should return an empty app list if directory does not exist", async() => {
+            expect.spyOn(client, "getContainers").andCall((done) => done(new Error()));
+
+            expect(await finder.apps()).toEqual([]);
+        });
+    });
+});
